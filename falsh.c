b79d8eefff4cb389010d044476ac6c057171f74d
Valdez <valdezr1@debian-9.3.0-template>
2018-04-25 16:34:56 -0700

    [-h] functional, and argument check when invoking falsh

--- falsh.c
+++ falsh.c
@@ -6,18 +6,60 @@
 //Prompts user that they have entered the Falcon Shell
 void prompt(){
 	printf("\n");
-	printf("*******************************\n\n");
+	printf("*******************************************\n\n");
 	
-		printf("  Welcome to the Falcon Shell\n\n");
+	printf("\tWelcome to the Falcon Shell\n\n");
 
-	printf("*******************************\n\n");
+	printf("*******************************************\n\n");
+}
+
+void promptHelp(){
+
+	printf("\n");
+	printf("*********************************************************************************************\n\n");
+	
+	printf("\t\t\tWelcome to the Falcon Shell Help File\n\n");
+
+	printf("*********************************************************************************************\n\n");
+
+	printf("\tAvailable Commands: \n");
+	printf("---------------------------------------------------------------------------------------------\n");
+
+	printf("\texit : Exit the program\n");
+	printf("\tpwd : Print the current working directory\n");
+	printf("\tcd [dir] : Change dirctory\n");
+	printf("\tsetpath <dir> [dir] ... [dir] : Sets the user's path\n");
+	printf("\thelp : List all built-in commands with short user-friendly descriptions\n");
+	printf("\n");
+	
+	printf("*********************************************************************************************\n\n");
+
 }
 
 int main(int argc, char** argv){
 	
-	//Prompt user
-	prompt();
-
+	
+	//Check for [-h] 
+	if (argc == 2) {
+		//Check if 2nd arg is "-h"
+		if (!strcmp(argv[1], "-h")) {
+			promptHelp();
+			exit(EXIT_SUCCESS);	
+		}
+		else {
+			printf("Unrecognized Flag\n");
+			exit(EXIT_FAILURE);	
+		}
+	}
+	//Check for more than two arguments to invoke falsh
+	else if(argc > 2){
+		printf("Invalid arguments\n");
+		exit(EXIT_FAILURE);
+	}
+	else {
+		//Prompt user
+		prompt();
+	}
 
 	return 0;
 }
