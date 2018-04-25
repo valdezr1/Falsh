b2d0541dd029e9c9047bc5e4e2aff425686183a6
Valdez <valdezr1@debian-9.3.0-template>
2018-04-25 15:55:42 -0700

    Prompted user with welcome message

--- falsh.c
+++ falsh.c
@@ -3,7 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+//Prompts user that they have entered the Falcon Shell
+void prompt(){
+	printf("\n");
+	printf("*******************************\n\n");
+	
+		printf("  Welcome to the Falcon Shell\n\n");
+
+	printf("*******************************\n\n");
+}
+
 int main(int argc, char** argv){
-	printf("yo\n");
+	
+	//Prompt user
+	prompt();
+
+
 	return 0;
 }
